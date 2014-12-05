3b528f92af6f9b082305134c54b39ece71489c73
Garrett Phoonswadi <garrett.phoonswadi@gmail.com>
2014-12-05 14:45:04 -0700

    ashda

--- Graph.java
+++ Graph.java
@@ -3,39 +3,18 @@ package PA5;
 import java.util.TreeMap;
 import java.util.Vector;
 
-
+// code is from CS200 recitation 14 thx 
 public class Graph {
-	/**
-	 * number of vertices in the graph
-	 */
+
 	private int numVertices;
 	
-	/**
-	 * number of edges in the graph
-	 */
+
 	private int numEdges;
 	
-	/**
-	 * I have added a boolean value to the class to help get you start with manipulating the constructor and the 
-	 * methods to differentiate between directed/undirected.
-	 */
 	private boolean directed;
 	
-	/**
-	 * For each vertex, we need to keep track of the edges, so for each edge, 
-	 * we need to store the second vertex and the edge weight. This can be done 
-	 * as a <key, value> pair, with the second vertex as the key, and the weight 
-	 * as the value. The TreeMap data structure is used to store a list of these
-	 * (key, value) pairs for each vertex, accessible as adjList.get(v)
-	 */
 	private Vector<TreeMap<Integer, Integer>> adjList;
-	
-	/**
-	 * Constructor for weighted graph </br>
-	 * Precondition: The number of vertices n should be greater than 0 </br>
-	 * Postcondition: Intializes the graph with n vertices
-	 * @param n
-	 */
+
 	public Graph(int n)
 	{
 		numVertices = n;
@@ -47,44 +26,21 @@ public class Graph {
 		}
 	}
 	
-	/**
-	 * Determines the number of vertices in the graph </br>
-	 * Precondition: None. </br>
-	 * @return number of vertices in the graph
-	 */
 	public int getNumVertices() 
 	{
 		return numVertices;
 	}
 	
-	/**Determines the number of edges in the graph </br>
-	 * Precondition: None.
-	 * @return the number of edges in the graph
-	 */
 	public int getNumEdges()
 	{
 		return numEdges;
 	}
 	
-	/**
-	 * Determines the weight of the edge between the vertices v and w </br>
-	 * Precondition: Edge must exist within the graph
-	 * @param v
-	 * @param w
-	 * @return The weight of the edge.
-	 */
 	public int getEdgeWeight(Integer v, Integer w)
 	{
 		return adjList.get(v).get(w);
 	}
 	
-	/**
-	 * Adds an edge from v to w with weight wgt to the graph </br>
-	 * Precondition The vertices contained within the edge e exist in the graph
-	 * @param v
-	 * @param w
-	 * @param wgt
-	 */
 	public void addEdge(Integer v, Integer w, int wgt) 
 	{
 		// Add the edge to both v's and w's adjacency list
@@ -94,10 +50,6 @@ public class Graph {
 		
 	}
 	
-	/**
-	 * Adds an edge to the graph
-	 * @param e
-	 */
 	public void addEdge(Edge e)
 	{
 		//Extract the vertices and weight from the edge e
@@ -106,12 +58,7 @@ public class Graph {
 		int weight = e.getWeight();
 		addEdge(v, w, weight);
 	}
-	
-	/**
-	 * Removes an edge from the graph </br>
-	 * Precondition: The vertices contained in the edge e exist in the graph
-	 * @param e
-	 */
+
 	public void removeEdge(Edge e)
 	{
 		// Extract the vertices from the edge e
@@ -124,25 +71,13 @@ public class Graph {
 		numEdges--;
 	}
 	
-	/**
-	 * Finds the edge connecting v and w. </br>
-	 * Precondition: The edge exists
-	 * @param v
-	 * @param w
-	 * @return The edge with the weight
-	 */
 	public Edge FindEdge(Integer v, Integer w)
 	{
 		int weight = adjList.get(v).get(w);
 		return new Edge(v, w, weight);
 	}
 	
-	/**
-	 * package access </br>
-	 * Returns the adjacency list for given vertex
-	 * @param v
-	 * @return The associated adjacency list
-	 */
+
 	TreeMap<Integer, Integer> getAdjList(Integer v)
 	{
 		return adjList.get(v);
@@ -167,34 +102,6 @@ public class Graph {
 		}
 		
 		return out;
-	}
-	
-	
-	public static void main(String[] args)
-	{
-		Graph g = new Graph(6);
-		g.addEdge(0, 2, 9);
-		g.addEdge(0, 3, 5);
-		g.addEdge(2, 3, 2);
-		g.addEdge(0, 1, 3);
-		g.addEdge(1, 4, 1);
-		g.addEdge(4, 5, 4);
-		g.addEdge(5, 1, 6);
-		
-		//System.out.println(g);
-	
-		Graph g1 = new Graph(6);
-		g1.addEdge(0, 2, 9);
-		g1.addEdge(0, 3, 5);
-		g1.addEdge(2, 3, 2);
-		g1.addEdge(0, 1, 3);
-		g1.addEdge(1, 4, 1);
-		g1.addEdge(1, 5, 6);
-		g1.addEdge(4, 5, 4);
-		
-		System.out.println(g1);
-		
-		
 	}
 	 
 }
